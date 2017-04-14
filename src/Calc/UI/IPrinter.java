56d06503f39856f0b96ff25b579c91ba22d348ae
almogaya1315 <liormatsliah1985@gmail.com>
2017-04-14 13:08:45 +0300

    delete irrelevant

--- src/Calc/UI/IPrinter.java
+++
@@ -1,8 +0,0 @@
-package Calc.UI;
-
-/**
- * Created by matsll on 4/13/2017.
- */
-public interface IPrinter {
-    void GetName();
-}
--- src/Calc/UI/Machine.java
+++
@@ -1,9 +0,0 @@
-package Calc.UI;
-
-/**
- * Created by matsll on 4/13/2017.
- */
-public abstract class Machine {
-    public int model;
-    public String name;
-}
