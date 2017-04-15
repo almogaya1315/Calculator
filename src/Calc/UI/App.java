5bcefdc89166f3e250686855bd697a4ddda1fe95
almogaya1315 <liormatsliah1985@gmail.com>
2017-04-15 21:46:28 +0300

    form builder column edit

--- src/Calc/UI/App.java
+++ src/Calc/UI/App.java
@@ -1,13 +1,36 @@
 package Calc.UI;
 
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.*;
 
 /**
  * Created by matsll on 4/13/2017.
  */
-public class App {
+public class App extends JPanel {
+    public static void main(String[] args){
+
+        //FormLayout
+
+        JFrame frame = new JFrame("App");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(new App());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public App(){
+        super(new BorderLayout());
+
+        DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+        builder.setBorder(BorderFactory.createEmptyBorder(3,3,3,3));
+        builder.appendColumn("fill:d:noGrow");
+
+        builder.append(new JTextField());
+    }
+
     private JPanel panelMain;
     private JTextField MainTextField;
     private JButton button1;
