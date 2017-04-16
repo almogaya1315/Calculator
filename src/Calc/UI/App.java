6355c390fbc75f9c9cc5c21ad91856005bee40b5
lior matsliah <liormatsliah1985@gmail.com>
2017-04-16 10:42:58 +0300

    basic calc. derived from --> http://www.java2s.com/Code/Java/Swing-Components/FormLayoutDefaultFormBuilderExample1.htm

--- src/Calc/UI/App.java
+++ src/Calc/UI/App.java
@@ -12,8 +12,6 @@ import java.awt.*;
 public class App extends JPanel {
     public static void main(String[] args){
 
-        //FormLayout
-
         JFrame frame = new JFrame("App");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(new App());
@@ -26,27 +24,86 @@ public class App extends JPanel {
 
         DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
         builder.setBorder(BorderFactory.createEmptyBorder(3,3,3,3));
+        builder.appendColumn("fill:max(pref; 75px)");
+        builder.appendColumn("2dlu");
+        builder.appendColumn("fill:max(pref; 75px)");
+        builder.appendColumn("2dlu");
+        builder.appendColumn("fill:max(pref; 75px)");
+        builder.appendColumn("2dlu");
+        builder.appendColumn("fill:max(pref; 75px)");
+
+        builder.append(new JTextField());
+        builder.nextLine();
+
+        builder.append(new JButton("7"));
+
+        builder.append(new JButton("8"));
+
+        builder.append(new JButton("9"));
+
+        builder.append(new JButton("+"));
+        builder.nextLine();
+
+        builder.append(new JButton("4"));
+
+        builder.append(new JButton("5"));
+
+        builder.append(new JButton("6"));
+
+        builder.append(new JButton("-"));
+        builder.nextLine();
+
+        builder.append(new JButton("1"));
+
+        builder.append(new JButton("2"));
+
+        builder.append(new JButton("3"));
+
+        builder.append(new JButton("*"));
+        builder.nextLine();
+
+        builder.append(new JButton("0"));
+
+        builder.append(new JButton("."));
+
+        builder.append(new JButton("="));
+
+        builder.append(new JButton("/"));
+        builder.nextLine();
+
+        add(builder.getPanel());
+
+        /*DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout(""));
+        builder.setBorder(BorderFactory.createEmptyBorder(3,3,3,3));
+        builder.appendColumn("fill:d:noGrow");
+        builder.appendColumn("left:4dlu:noGrow");
+        builder.appendColumn("fill:d:noGrow");
+        builder.appendColumn("left:4dlu:noGrow");
+        builder.appendColumn("fill:d:noGrow");
+        builder.appendColumn("left:4dlu:noGrow");
         builder.appendColumn("fill:d:noGrow");
 
         builder.append(new JTextField());
+        builder.nextLine();
+
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.nextLine();
+
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.nextLine();
+
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.append(new JButton());
+        builder.nextLine();*/
     }
 
     private JPanel panelMain;
-    private JTextField MainTextField;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
-    private JButton button16;
 }
